7a71129b0f737ada7da31fcbf7dd9045327ebe74
SyNthw8ve <nunofmcarrico@gmail.com>
2020-08-23 21:28:54 +0100

    Hardskill schema

--- src/hardskill/dtos/create-hardskill.dto.ts
+++ src/hardskill/dtos/create-hardskill.dto.ts
@@ -1,5 +1,9 @@
 import { Hardskill } from "../schemas/hardskill.schema";
+import { ObjectType , Field } from '@nestjs/graphql';
 
+@ObjectType()
 export class CreateHardskillDto {
+
+    @Field(type => String, { nullable: false })
     name: Hardskill['name'];
 }
