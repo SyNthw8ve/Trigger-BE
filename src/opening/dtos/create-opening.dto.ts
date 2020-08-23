a14fd7c5161d02ab4f7a9abedc1d08edd8300f51
SyNthw8ve <nunofmcarrico@gmail.com>
2020-08-23 21:57:28 +0100

    Opening schema

--- src/opening/dtos/create-opening.dto.ts
+++ src/opening/dtos/create-opening.dto.ts
@@ -2,10 +2,21 @@ import { Availability } from "src/common/schemas/availability.schema";
 import { Locale } from "src/common/schemas/locale.schema";
 import { Opening } from "../schemas/opening.schema";
 
+import { InputType, Field, ID, Float } from '@nestjs/graphql';
+
+@InputType()
 export class CreateOpeningDto {
+
+    @Field(type => ID, { nullable: false })
     project: Opening['project'];
+
+    @Field(type => String, { nullable: false })
     area: Opening['area'];
+
+    @Field(type => Float, { nullable: false })
     remuneration: Opening['remuneration'];
+
+    @Field(type => Locale, { nullable: true })
     location?: Locale;
 
     // languages;
--- src/opening/schemas/opening.schema.ts
+++ src/opening/schemas/opening.schema.ts
@@ -10,51 +10,72 @@ import { Project } from 'src/project/schemas/project.schema';
 import { Softskill, SoftskillSchema } from 'src/softskill/schemas/softskill.schema';
 import { User } from 'src/user/schemas/user.schema';
 
+import { ObjectType, Field, ID, Float, registerEnumType } from '@nestjs/graphql';
+
 export enum ApplicationType {
     Manual = "spontaneous",
     Match = "match"
 }
 
+registerEnumType(ApplicationType, { name: 'ApplicationType' });
+
+@ObjectType()
 export class Application {
+
+    @Field(type => ApplicationType, { nullable: false })
     @Prop({ required: true })
     type: ApplicationType;
 
+    @Field(type => ID, { nullable: true })
     @Prop({ type: SchemaTypes.ObjectId, ref: Match.name, required: true })
     match?: Match['_id'];
 
+    @Field(type => ID, { nullable: false })
     @Prop({ type: SchemaTypes.ObjectId, ref: User.name, required: true })
     user: User['_id'];
 }
 
+@ObjectType()
 @Schema()
 export class Opening extends Document {
+
+    @Field(type => String, { nullable: false })
     @Prop({ required: true })
     area: string;
 
+    @Field(type => Float, { nullable: false })
     @Prop({ required: true })
     remuneration: number;
 
+    @Field(type => Locale, { nullable: true })
     @Prop({ type: LocaleSchema, required: false })
     location?: Locale;
 
+    @Field(type => [ID])
     @Prop([{ type: SchemaTypes.ObjectId, ref: Language.name }])
     languages: Language['_id'][];
 
+    @Field(type => [ID])
     @Prop([{ type: SchemaTypes.ObjectId, ref: Softskill.name }])
     softskills: Softskill['_id'][];
 
+    @Field(type => [ID])
     @Prop([{ type: SchemaTypes.ObjectId, ref: Hardskill.name }])
     hardskills: Hardskill['_id'][];
 
+    @Field(type => Availability, { nullable: false })
     @Prop({ type: AvailabilitySchema, required: true })
     availability: Availability;
 
+    @Field(type => [Application])
     @Prop([Application])
     applications: Application[];
 
+    @Field(type => [ID])
     @Prop([{ type: SchemaTypes.ObjectId, ref: 'Participation' }])
     participations: Participation['_id'][];
 
+    @Field(type => ID, { nullable: false })
     @Prop({ type: SchemaTypes.ObjectId, ref: 'Project', required: true })
     project: Project['_id'];
 }
