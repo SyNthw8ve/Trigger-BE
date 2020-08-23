837d89d0e7d9f486e0dcbf21021535776cdb9cc4
SyNthw8ve <nunofmcarrico@gmail.com>
2020-08-23 21:44:27 +0100

    Language schema

--- src/language/dtos/create-language.dto.ts
+++ src/language/dtos/create-language.dto.ts
@@ -1,5 +1,9 @@
 import { Language } from "../schemas/language.schema";
+import { InputType, Field } from '@nestjs/graphql';
 
+@InputType()
 export class CreateLanguageDto {
+
+    @Field(type => String, { nullable: false })
     name: Language['name'];
 }
--- src/language/schemas/language.schema.ts
+++ src/language/schemas/language.schema.ts
@@ -1,8 +1,12 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
+import { ObjectType, Field } from '@nestjs/graphql';
 
+@ObjectType()
 @Schema()
 export class Language extends Document {
+
+    @Field(type => String, { nullable: false })
     @Prop({ required: true })
     name: String;
 }
