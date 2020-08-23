c05b0f8acc5565066d80ddeb448f1cb25a820cb2
SyNthw8ve <nunofmcarrico@gmail.com>
2020-08-23 22:13:41 +0100

    Softskill schema

--- src/softskill/schemas/softskill.schema.ts
+++ src/softskill/schemas/softskill.schema.ts
@@ -1,8 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
+import { ObjectType, Field } from '@nestjs/graphql';
+
+@ObjectType()
 @Schema()
 export class Softskill extends Document {
+
+    @Field(type => String, { nullable: false })
     @Prop({ required: true })
     name: String;
 }
