d685f5d8f8d27f97e9eda497b5c00ba2e8ae1c8f
SyNthw8ve <nunofmcarrico@gmail.com>
2020-08-23 22:00:19 +0100

    Participation schema

--- src/participation/dtos/participation.dto.ts
+++ src/participation/dtos/participation.dto.ts
@@ -1,7 +1,17 @@
 import { Participation } from "../schemas/participation.schema";
+import { Task } from "../../common/schemas/task";
 
+import { InputType, Field, ID} from '@nestjs/graphql';
+
+@InputType()
 export class ParticipationDto {
+
+    @Field(type => ID, { nullable: false })
     opening: Participation['opening'];
+
+    @Field(type => ID, { nullable: false })
     phase: Participation['phase'];
+
+    @Field(type => [Task], { nullable: false })
     tasks: Participation['tasks'];
 }
--- src/participation/schemas/participation.schema.ts
+++ src/participation/schemas/participation.schema.ts
@@ -4,14 +4,21 @@ import { Opening } from 'src/opening/schemas/opening.schema';
 import { Phase } from 'src/phase/schemas/phase.schema';
 import { Task, TaskSchema } from "src/common/schemas/task";
 
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+
+@ObjectType()
 @Schema()
 export class Participation extends Document {
+
+    @Field(type => ID, { nullable: false })
     @Prop({ type: SchemaTypes.ObjectId, ref: Opening.name, required: true })
     opening: Opening['_id'];
 
+    @Field(type => ID, { nullable: false })
     @Prop({ type: SchemaTypes.ObjectId, ref: Phase.name, required: true })
     phase: Phase['_id'];
 
+    @Field(type => [Task], { nullable: false })
     @Prop([{ type: TaskSchema, required: true }])
     tasks: Task[];
 }
