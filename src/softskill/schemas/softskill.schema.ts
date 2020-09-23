c3b35a5956e70678717a998f4569b4b581e086bd
David Pinto <daxtery11@gmail.com>
2020-09-23 16:14:45 +0100

    Add _id to the GraphQL output of Softskill

--- src/softskill/schemas/softskill.schema.ts
+++ src/softskill/schemas/softskill.schema.ts
@@ -1,11 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
-import { ObjectType, Field } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
 
 @ObjectType()
 @Schema()
 export class Softskill extends Document {
+    @Field(() => ID)
+    readonly _id: string;
 
     @Field(type => String, { nullable: false })
     @Prop({ required: true })
