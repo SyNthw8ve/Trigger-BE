87f19fbbb6729a5c877cc860d7780bd55fa3abcf
SyNthw8ve <nunofmcarrico@gmail.com>
2020-10-07 15:21:28 +0100

    Softskills quiz service

--- src/soft-quiz/soft-quiz.module.ts
+++ src/soft-quiz/soft-quiz.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SoftSkillQuestions, SoftSkillQuestionSchema } from './schemas/soft-quiz.schema';
+import { SoftSkillQuestionService } from './soft-quiz.service';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: SoftSkillQuestions.name, schema: SoftSkillQuestionSchema }])],
-    exports: [],
-    providers: []
+    exports: [SoftSkillQuestionService],
+    providers: [SoftSkillQuestionService]
 })
 export class SoftQuizModule { }
---
+++ src/soft-quiz/soft-quiz.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { SoftSkillQuestions } from './schemas/soft-quiz.schema'
+
+@Injectable()
+export class SoftSkillQuestionService {
+
+    constructor(@InjectModel(SoftSkillQuestions.name) private questionModel: Model<SoftSkillQuestions>) { }
+
+    async getQuiz() : Promise<SoftSkillQuestions[]> {
+
+        const quiz = await this.questionModel.find();
+        
+        this.shuffleQuiz(quiz);
+
+        return quiz;
+    }
+
+    private shuffleQuiz(quiz: SoftSkillQuestions[]) {
+
+        for(let i = quiz.length - 1; i > 0; i--) {
+
+            const j = Math.floor(Math.random() * i);
+            const temp = quiz[i];
+
+            quiz[i] = quiz[j];
+            quiz[j] = temp; 
+        }
+    }
+}
