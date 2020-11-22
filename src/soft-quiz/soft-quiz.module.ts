1a11ae816fe6d5f20bfd1ad65cdeca36919b14b6
David Pinto <daxtery11@gmail.com>
2020-11-22 15:02:32 +0000

    Add SoftSkillQuestionsResolver to module

--- src/soft-quiz/soft-quiz.module.ts
+++ src/soft-quiz/soft-quiz.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SoftSkillQuestion, SoftSkillQuestionSchema } from './schemas/soft-quiz.schema';
+import { SoftSkillQuestionsResolver } from './soft-quiz.resolver';
 import { SoftSkillQuestionService } from './soft-quiz.service';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: SoftSkillQuestion.name, schema: SoftSkillQuestionSchema }])],
+    providers: [SoftSkillQuestionService, SoftSkillQuestionsResolver],
     exports: [SoftSkillQuestionService],
-    providers: [SoftSkillQuestionService],
 })
 export class SoftQuizModule { }
