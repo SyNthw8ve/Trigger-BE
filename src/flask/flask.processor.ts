9c1a75d0dc0500b43d0e94fec253895f4032415f
David Pinto <daxtery11@gmail.com>
2020-12-13 19:01:53 +0000

    Log errors from flask

--- src/flask/flask.processor.ts
+++ src/flask/flask.processor.ts
@@ -2,6 +2,8 @@ import { Processor, Process } from '@nestjs/bull';
 import { HttpService } from '@nestjs/common';
 import { Job } from 'bull';
 import { beAiConnection } from 'src/config';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Processor('flask')
 export class FlaskProcessor {
@@ -12,16 +14,23 @@ export class FlaskProcessor {
     async computeUserMatches(job: Job<unknown>) {
         let user_id = job.data["user_id"];
 
-        let result = this.httpService.post(`${beAiConnection}/user_match/${user_id}`);
-        result.subscribe(val => console.info(val));
+        // FIXME: Handle errors
+        this.httpService.post(`${beAiConnection}/user_match/${user_id}`).subscribe(
+            val => console.info(val),
+            error => console.error(error)
+        )
     }
 
     @Process('update_user')
     async updateUserMatches(job: Job<unknown>) {
         let user_id = job.data["user_id"];
 
-        let resut = this.httpService.put(`${beAiConnection}/user_match/${user_id}`);
-        resut.subscribe(val => console.info(val));
+        // FIXME: Handle errors
+        
+        this.httpService.put(`${beAiConnection}/user_match/${user_id}`).subscribe(
+            val => console.info(val),
+            error => console.error(error)
+        )
     }
 
     @Process('calculate_score')
@@ -29,28 +38,47 @@ export class FlaskProcessor {
         let user_id = job.data["user_id"];
         let opening_id = job.data["opening_id"];
 
-        let resut = this.httpService.put(`${beAiConnection}/score/${user_id}/${opening_id}`);
-        resut.subscribe(val => console.info(val));
+        // FIXME: Handle errors
+
+        this.httpService.put(`${beAiConnection}/score/${user_id}/${opening_id}`).subscribe(
+            val => console.info(val),
+            error => console.error(error)
+        )
     }
 
     @Process('insert_opening')
     async insertOpeningToCluster(job: Job<unknown>) {
         let opening_id = job.data["opening_id"];
 
-        this.httpService.put(`${beAiConnection}/opening/${opening_id}`);
+        // FIXME: Handle errors
+
+        this.httpService.put(`${beAiConnection}/opening/${opening_id}`).subscribe(
+            val => console.info(val),
+            error => console.error(error)
+        )
     }
 
     @Process('update_opening')
     async updateOpeningInCluster(job: Job<unknown>) {
         let opening_id = job.data["opening_id"];
 
-        this.httpService.put(`${beAiConnection}/opening/${opening_id}`);
+        // FIXME: Handle errors
+
+        this.httpService.put(`${beAiConnection}/opening/${opening_id}`).subscribe(
+            val => console.info(val),
+            error => console.error(error)
+        )
     }
 
     @Process('delete_opening')
     async deleteOpeningInCluster(job: Job<unknown>) {
         let opening_id = job.data["opening_id"];
 
-        this.httpService.delete(`${beAiConnection}/opening/${opening_id}`);
+        // FIXME: Handle errors
+        
+        this.httpService.delete(`${beAiConnection}/opening/${opening_id}`).subscribe(
+            val => console.info(val),
+            error => console.error(error)
+        )
     }
 }
