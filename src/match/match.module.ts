528c4d32c80eb75c6b190f7296162cc585c679da
David Pinto <daxtery11@gmail.com>
2020-12-13 19:03:49 +0000

    MatchService now consumes ServerMatches

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -4,10 +4,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Match, MatchSchema } from './schemas/match.schema';
 import { FlaskModule } from 'src/flask/flask.module';
+import { ServerMatch, ServerMatchSchema } from './schemas/server-match.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Match.name, schema: MatchSchema }]),
+    MongooseModule.forFeature([
+      { name: Match.name, schema: MatchSchema },
+      { name: ServerMatch.name, schema: ServerMatchSchema }
+    ]),
     FlaskModule,
   ],
   controllers: [MatchController,],
