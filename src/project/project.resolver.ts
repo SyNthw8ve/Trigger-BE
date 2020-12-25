a7f3bdc17672ea4bc613613fbe7e69f8a0bcd15d
David Pinto <daxtery11@gmail.com>
2020-12-25 21:33:55 +0000

    Project can now fetch openings (not just Ids...)

--- src/project/project.resolver.ts
+++ src/project/project.resolver.ts
@@ -8,14 +8,17 @@ import { User } from '../user/schemas/user.schema';
 import { InstitutionService } from '../institution/institution.service';
 import { Institution } from '../institution/schemas/institution.schema';
 import { DraftProject } from './schemas/draft-project.schema';
+import { Opening } from 'src/opening/schemas/opening.schema';
+import { OpeningService } from 'src/opening/opening.service';
 
 @Resolver(of => Project)
 export class ProjectResolver {
 
     constructor(private readonly projectService: ProjectService,
         private readonly userService: UserService,
-        private readonly institutionService: InstitutionService) { }
-        
+        private readonly institutionService: InstitutionService,
+        private readonly openingService: OpeningService) { }
+
     @Mutation(returns => Project)
     async newProject(@Args('project') project: CreateProjectDto): Promise<Project> {
 
@@ -23,31 +26,44 @@ export class ProjectResolver {
     }
 
     @Query(returns => Project)
-    async getProject(@Args('id') id: String) : Promise<Project> {
+    async getProject(@Args('id') id: String): Promise<Project> {
 
         return await this.projectService.findWithId(id);
     }
 
-    @ResolveField('currentTeam' , returns => [User])
-    async getTeam(@Parent() project: Project) : Promise<User[]> {
+    @ResolveField('openings', returns => [Opening])
+    async getOpenings(@Parent() project: Project): Promise<Opening[]> {
+
+        const ids = project.openings;
+
+        return await Promise.all(
+            ids.map(id =>
+                this.openingService.findWithId(id)
+            )
+        );
+
+    }
+
+    @ResolveField('currentTeam', returns => [User])
+    async getTeam(@Parent() project: Project): Promise<User[]> {
 
         return await this.userService.findManyWithId(project.currentTeam);
     }
 
-    @ResolveField('admins' , returns => [User])
-    async getAdmins(@Parent() project: Project) : Promise<User[]> {
+    @ResolveField('admins', returns => [User])
+    async getAdmins(@Parent() project: Project): Promise<User[]> {
 
         return await this.userService.findManyWithId(project.admins);
     }
 
     @ResolveField('manager', returns => User)
-    async getManager(@Parent() project: Project) : Promise<User> {
+    async getManager(@Parent() project: Project): Promise<User> {
 
         return await this.userService.findWithId(project.manager);
     }
 
     @ResolveField('institution', returns => Institution, { nullable: true })
-    async getInstitution(@Parent() project: Project) : Promise<Institution> {
+    async getInstitution(@Parent() project: Project): Promise<Institution> {
 
         return await this.institutionService.findWithId(project.institution);
     }
